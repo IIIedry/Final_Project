e3ba549ffc5fe7c77bace64daf72665bf859e3e8
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 15:06:48 +0600

    billing part on create function written

--- app/src/main/java/com/example/shop/fragments/shopping/BillingFragment.kt
+++ app/src/main/java/com/example/shop/fragments/shopping/BillingFragment.kt
@@ -1,4 +1,41 @@
 package com.example.shop.fragments.shopping
 
-class BillingFragment {
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shop.data.order.CartProduct
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+
+@AndroidEntryPoint
+class BillingFragment : Fragment() {
+    private lateinit var binding: FragmentBillingBinding
+    private val addressAdapter by lazy { AddressAdapter() }
+    private val billingProductsAdapter by lazy { BillingProductsAdapter() }
+    private val billingViewModel by viewModels<BillingViewModel>()
+    private val args by navArgs<BillingFragmentArgs>()
+    private var products = emptyList<CartProduct>()
+    private var totalPrice = 0f
+
+    private var selectedAddress: Address? = null
+    private val orderViewModel by viewModels<OrderViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        products = args.products.toList()
+        totalPrice = args.totalPrice
+    }
+
 }
