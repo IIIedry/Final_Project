c7fc19c59c20e1c3a3f5173bafccf0449eeb44b0
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 11:12:23 +0600

    fixing error and filling address and user

--- app/src/main/java/com/example/shop/data/order/Address.kt
+++ app/src/main/java/com/example/shop/data/order/Address.kt
@@ -1,2 +1,19 @@
 package com.example.shop.data.order
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Address(
+    val addressTitle: String,
+    val fullName: String,
+    val street: String,
+    val phone: String,
+    val city: String,
+    val state: String
+): Parcelable {
+
+    constructor(): this("","","","","","")
+}
+//    constructor(): this("","IIIedry King","Abai 45A","+77471664865","Taraz","Kazakhstan")
+//}
--- app/src/main/java/com/example/shop/data/order/User.kt
+++ app/src/main/java/com/example/shop/data/order/User.kt
@@ -1,19 +1,10 @@
 package com.example.shop.data.order
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Address(
-    val addressTitle: String,
-    val fullName: String,
-    val street: String,
-    val phone: String,
-    val city: String,
-    val state: String
-): Parcelable {
-
-    constructor(): this("","","","","","")
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    var imagePath: String = ""
+){
+    constructor(): this("IIIedry","King","Iliyas@mail.ru","")
 }
-//    constructor(): this("","IIIedry King","Abai 45A","+77471664865","Taraz","Kazakhstan")
-//}
