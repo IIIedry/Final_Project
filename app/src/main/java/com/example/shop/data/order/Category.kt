83950feb447f4f8036939811ef31a5fa90a62ffb
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 11:08:23 +0600

    category for clothes to choose from

--- app/src/main/java/com/example/shop/data/order/Category.kt
+++ app/src/main/java/com/example/shop/data/order/Category.kt
@@ -1,2 +1,10 @@
 package com.example.shop.data.order
 
+sealed class Category(val category: String) {
+
+    object Shirts: Category("T-Shirts")
+    object Jackets: Category("Jackets")
+    object Pants: Category("Pants")
+    object Sneakers: Category("Sneakers")
+    object Hats: Category("Hats")
+}
