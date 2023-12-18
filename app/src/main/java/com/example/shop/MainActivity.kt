5e8e831a133d80c7b7d864a3ec3af7218dbb3192
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 22:39:35 +0600

    constant and change main activity

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -1,29 +1,9 @@
 package com.example.shop
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.shop.ui.theme.ShopTheme
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ShopTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Shop()
-                }
-            }
-        }
-    }
+@HiltAndroidApp
+class MainActivity: Application() {
+
 }
