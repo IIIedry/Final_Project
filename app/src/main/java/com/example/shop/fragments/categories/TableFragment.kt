afd1d22969d7d63ab26b313412c3f9c0ab170e57
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 13:35:54 +0600

    add onviewCreated

--- app/src/main/java/com/example/shop/fragments/categories/TableFragment.kt
+++ app/src/main/java/com/example/shop/fragments/categories/TableFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.navigation.Navigation.findNavController
 import com.example.shop.R
 
 private val TAG = "MainCategoryFragment"
@@ -26,3 +28,65 @@ class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
         binding = FragmentMainCategoryBinding.inflate(inflater)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSpecialProductsRv()
+        setupBestDealsRv()
+        setupBestProducts()
+
+        specialProductsAdapter.onClick = {
+            val b = Bundle().apply { putParcelable("product", it) }
+            findNavController().navigate(R.id.action_homeFragment_to_productDetailsFragment, b)
+        }
+
+        bestDealsAdapter.onClick = {
+            val b = Bundle().apply { putParcelable("product", it) }
+            findNavController().navigate(R.id.action_homeFragment_to_productDetailsFragment, b)
+        }
+
+        bestProductsAdapter.onClick = {
+            val b = Bundle().apply { putParcelable("product", it) }
+            findNavController().navigate(R.id.action_homeFragment_to_productDetailsFragment, b)
+        }
+
+
+        private fun setupBestProducts() {
+            bestProductsAdapter = BestProductsAdapter()
+            binding.rvBestProducts.apply {
+                layoutManager =
+                    GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+                adapter = bestProductsAdapter
+            }
+        }
+
+        private fun setupBestDealsRv() {
+            bestDealsAdapter = BestDealsAdapter()
+            binding.rvBestDealsProducts.apply {
+                layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                adapter = bestDealsAdapter
+            }
+        }
+
+        private fun hideLoading() {
+            binding.mainCategoryProgressbar.visibility = View.GONE
+        }
+
+        private fun showLoading() {
+            binding.mainCategoryProgressbar.visibility = View.VISIBLE
+
+        }
+
+        private fun setupSpecialProductsRv() {
+            specialProductsAdapter = SpecialProductsAdapter()
+            binding.rvSpecialProducts.apply {
+                layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                adapter = specialProductsAdapter
+            }
+        }
+
+    }
+}
