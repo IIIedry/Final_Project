dd315a8e63da85a5eb0347e65f3ce6637cf8263a
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 15:10:03 +0600

    fragment card add new item added

--- app/src/main/java/com/example/shop/fragments/shopping/CartFragment.kt
+++ app/src/main/java/com/example/shop/fragments/shopping/CartFragment.kt
@@ -1,4 +1,74 @@
 package com.example.shop.fragments.shopping
 
-class CartFragment {
+import android.app.AlertDialog
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shop.R
+import kotlinx.coroutines.flow.collectLatest
+
+class CartFragment : Fragment(R.layout.fragment_cart) {
+    private lateinit var binding: FragmentCartBinding
+    private val cartAdapter by lazy { CartProductAdapter() }
+    private val viewModel by activityViewModels<CartViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCartBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupCartRv()
+
+        var totalPrice = 0f
+        lifecycleScope.launchWhenStarted {
+            viewModel.productsPrice.collectLatest { price ->
+                price?.let {
+                    totalPrice = it
+                    binding.tvTotalPrice.text = "$ $price"
+                }
+            }
+        }
+
+        cartAdapter.onProductClick = {
+            val b = Bundle().apply {
+                putParcelable("product", it.product)
+            }
+            findNavController().navigate(R.id.action_cartFragment_to_productDetailsFragment, b)
+        }
+
+        cartAdapter.onPlusClick = {
+            viewModel.changeQuantity(it, FirebaseCommon.QuantityChanging.INCREASE)
+        }
+
+        cartAdapter.onMinusClick = {
+            viewModel.changeQuantity(it, FirebaseCommon.QuantityChanging.DECREASE)
+        }
+
+        binding.buttonCheckout.setOnClickListener {
+            val action = CartFragmentDirections.actionCartFragmentToBillingFragment2(
+                totalPrice,
+                cartAdapter.differ.currentList.toTypedArray(),
+                true
+            )
+            findNavController().navigate(action)
+        }
+
+    }
 }
