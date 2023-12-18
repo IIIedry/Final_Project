038f9a87ffd6e8e439239c45f9ff1f7818fb79e9
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 13:34:26 +0600

    entry point fragment first part

--- app/src/main/java/com/example/shop/fragments/categories/TableFragment.kt
+++ app/src/main/java/com/example/shop/fragments/categories/TableFragment.kt
@@ -1,2 +1,28 @@
 package com.example.shop.fragments.categories
 
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shop.R
+
+private val TAG = "MainCategoryFragment"
+
+@AndroidEntryPoint
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
+
+    private lateinit var binding: FragmentMainCategoryBinding
+    private lateinit var specialProductsAdapter: SpecialProductsAdapter
+    private lateinit var bestDealsAdapter: BestDealsAdapter
+    private lateinit var bestProductsAdapter: BestProductsAdapter
+    private val viewModel by viewModels<MainCategoryViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
+    }
