1086d1064ff224eeb8758d008574f4f5a5bffd26
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 17:28:59 +0600

    all order file include adapter filling

--- app/src/main/java/com/example/shop/fragments/settings/AllOrderFragment.kt
+++ app/src/main/java/com/example/shop/fragments/settings/AllOrderFragment.kt
@@ -14,6 +14,7 @@ import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kelineyt.adapters.AllOrdersAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 
