763a404a2f0f04eae0750d5ce675c476a44013a1
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-12-18 11:03:26 +0600

    data class for card adding

--- app/src/main/java/com/example/shop/data/order/CartProdect.kt
+++ app/src/main/java/com/example/shop/data/order/CartProdect.kt
@@ -1,2 +1,14 @@
 package com.example.shop.data.order
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class CartProduct(
+    val product: Product,
+    val quantity: Int,
+    val selectedColor: Int? = null,
+    val selectedSize: String? = null
+): Parcelable {
+    constructor() : this(Product(), 1, null, null)
+}
